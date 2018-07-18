510ca1409407fe4a40a27e551344dde9a528f265
dipikabaad <dipika.baad@gmail.com>
2018-07-18 09:02:18 +0200

    Added bootstrap and initial transition

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,30 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div style="margin-top: 200px">
+    <button class="btn btn-primary" @click="show= !show">Toggle</button>
+    <transition>
+    <p v-if="show">Lorem ipsum dolar sit amet</p>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data(){
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      show: false
+    };
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .v-enter, .v-leave-to{
+    opacity: 0;
+  }
+  .v-enter-active, .v-leave-active{
+    transition: opacity 5s; 
+  }
+ 
 </style>
+
