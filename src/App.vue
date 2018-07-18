fe50a2e948e9e53691ac3e1a288d6a339ae4a820
dipikabaad <dipika.baad@gmail.com>
2018-07-18 17:49:56 +0200

    transitioning multiple elements

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,50 @@
 <template>
   <div style="margin-top: 200px">
-    <button class="btn btn-primary" @click="changeShape">Switch Shapes</button>
+    <button class="btn btn-primary" @click="addRandom">Add Random Number</button>&nbsp;
+    <button class="btn btn-primary" @click="removeRandom">Remove Random Number</button>
     <br><br>
-    <transition name="fade" mode="out-in" appear>
-    <component :is="shape"></component>
-    </transition>
+    <transition-group name="fade">
+    <span class="list-item" v-for="n in numbers" :key="n">{{n}}</span>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import Square from './Square.vue';
-import Circle  from './Circle.vue';
+
 export default {
   data(){
     return {
-      shape: 'appSquare'
+      numbers: [1,2,3,4,5,6,7,8,9,10]
     };
   },
  methods:{
-  changeShape(){
-    if(this.shape == 'appSquare'){
-      this.shape = 'appCircle'
-    } else {
-      this.shape = 'appSquare'
-    }
-  }
- },
- components: {
-  appSquare : Square,
-  appCircle: Circle
+  getRandomIndex(){
+    return Math.floor(Math.random() * this.numbers.length);
+   },
+   addRandom(){
+    let randomNumber = Math.floor(Math.random() * 100);
+    this.numbers.splice(this.getRandomIndex(), 0, randomNumber);
+   },
+   removeRandom(){
+    this.numbers.splice(this.getRandomIndex(),1)
+   }
  }
 }
 </script>
 
 <style>
+  .list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
   .fade-enter-active, .fade-leave-active {
-    transition: opacity 1s ease;
+    transition: all 1s;
   }
   .fade-enter, .fade-leave-to {
     opacity: 0;
+    transition: rotate(45deg) translate(-32px);
   }
+
 </style>
 
 
