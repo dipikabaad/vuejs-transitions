2d3cff12b9f91e67f8b41e5f6001dd3a59ebd94a
dipikabaad <dipika.baad@gmail.com>
2018-07-18 16:58:08 +0200

    transitioning between elementa

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,8 @@
   <div style="margin-top: 200px">
     <button class="btn btn-primary" @click="show= !show">Toggle</button>
     <transition name="fade">
-    <p v-if="show" style="width: 200px;">Lorem ipsum dolar sit amet</p>
-    </transition>
-    <transition name="pulse-fade" type="animation">
-    <p v-if="show" style="width: 200px;">Lorem ipsum dolar sit amet</p>
+    <p v-if="show" key="lorem-ipsum">Lorem ipsum dolar sit amet</p>
+    <p v-else key="nohting-around-here">Nothing to see here</p>
     </transition>
   </div>
 </template>
@@ -21,35 +19,11 @@ export default {
 }
 </script>
 <style>
-  .fade-enter, fade-leave-to, pulse-fade-enter, pulse-fade-leave-to{
-    opacity: 0;
-  }
-  .fade-enter-active, fade-leave-active {
-    transition: opacity 1s;
-  }
-
-  .pulse-fade-enter-active{
-    transition: opacity .5s;
-    animation: pulse 1s;
-  }
-  .pulse-fade-leave{
-    opacity: 1;
-  }
-  .pulse-fade-leave-active{
-    transition: opacity 4s;
-    animation: pulse 1s;
-  }
-
-  @keyframe pulse{
-    from {
-      transform: scaled(1,1,1);
-    }
-    50% {
-      transform: scaled(1.2,1.2,1.2);
-    }
-    to {
-      transform: scaled(1,1,1);
-    }
-  }
+ .fade-enter, fade-leave-to {
+    opacity: 0
+ }
+ .fade-enter-active, .fade-leave-active {
+  transition: opacity 1s;
+ }
 </style>
 
